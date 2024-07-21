056eeafc16e75a36e6fbedca529e956d94e165b8
Vladimir Zykov <vova@home>
2024-07-21 18:41:24 +0300

    ~

--- eloquent_ruby_code/01/ex_case.rb
+++ eloquent_ruby_code/01/ex_case.rb
@@ -1,24 +1,24 @@
 ##
 
 def case_no_semi(title)
-##start no_semi
-  case title
-  when 'War And Peace'
-    puts 'Tolstoy'
-  when 'Romeo And Juliet'
-    puts 'Shakespeare'
+  # #start no_semi
+  author = case title
+  when "War And Peace"
+    puts "Tolstoy"
+  when "Romeo And Juliet"
+    puts "Shakespeare"
   else
     puts "Don't know"
   end
-##end no_semi
+  # #end no_semi
 end
 
 def case_semi(title)
-##start semi
-  case title; 
-  when 'War And Peace';    puts 'Tolstoy'
-  when 'Romeo And Juliet'; puts 'Shakespeare'
-  else; puts "Don't know"
+  # #start semi
+  case title
+  when "War And Peace" then puts "Tolstoy"
+  when "Romeo And Juliet" then puts "Shakespeare"
+  else puts "Don't know"
   end
-##end semi
+  # #end semi
 end
--- ex.rb
+++ ex.rb
@@ -2,14 +2,8 @@
 
 ["pp"].each { |m| require m }
 
-class TestClass
-  def initialize(count)
-    @@a = defined?(@@a) ? @@a + 1 : 0
-    @c = @@a
-    @d = [a: {b: count}, c: :d] * count
-  end
-end
+["courier", "times roman", "helvetica"].each { |f| puts f }
 
-pp TestClass.new(2), STDOUT, 120
-pp TestClass.new(3), $>, 120
-pp TestClass.new(4), $>, 120
+wrds = %w[one two three]
+
+haba = {daba: "kata", laba: "pusa"}
